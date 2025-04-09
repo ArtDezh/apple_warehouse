da146868967ba0627bba740c1d7b4a466b046371
ArtDezh <artur2134@mail.ru>
2025-04-09 08:28:38 +0300

    Созданы новые классы под критерии поиска яблок

---
+++ src/main/java/GreenAppleSearchCriteria.java
@@ -0,0 +1,6 @@
+public class GreenAppleSearchCriteria implements AppleSearchCriteria {
+    @Override
+    public boolean test(Apple apple) {
+        return "green".equals(apple.getColor());
+    }
+}
